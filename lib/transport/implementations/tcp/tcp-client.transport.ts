e810d9869c4acc3a82d71440aadbeffe0c81a8d0
Fugazzii <sichinavailia@gmail.com>
2023-09-28 11:55:11 +0400

    Implemented TransportClient

--- lib/transport/implementations/tcp/tcp-client.transport.ts
+++ lib/transport/implementations/tcp/tcp-client.transport.ts
@@ -1,31 +1,91 @@
-import { Address, Callback, ITransport, ITransportClient, ITransportServer } from "../..";
+import { Address, ITransportClient, JsonRpcRequest, JsonRpcResponse } from "../..";
+import net from "net";
+
+type ResponseResolver = (value: JsonRpcResponse | PromiseLike<JsonRpcResponse>) => void;
 
 export class TcpClient implements ITransportClient {
 
-    public constructor(
-        { hostname, port }: Address,
-        private readonly transportServer: ITransport
-    ) {
-        Bun.connect({
-            hostname,
-            port,
-            socket: {
-                data(socket, data) {
-                    console.log("client: ", data);
-                }
+    private readonly socket: net.Socket; 
+    private readonly pendingRequests: Map<string, ResponseResolver>;
+
+    public constructor({ hostname, port }: Address) {
+        this.socket = net.connect({ port, host: hostname });
+        this.pendingRequests = new Map();
+
+        this._listenForResponses();
+    }
+
+    /** Constructor for client */
+    public static connect(addr: Address): TcpClient {
+        return new TcpClient(addr);
+    }
+
+    /**
+     * This method recieves method name of RPC method
+     * Generates unique ID so we can identify its response by matching IDs
+     * Sends request object to server and stores its Promise resolve callback in Map
+     */
+    public send(method: string): Promise<JsonRpcResponse> {
+        
+        let currentTime = Date.now();
+        let uniqueId = String(currentTime) + this._generateRequestId();
+
+        const jsonRpcRequest: JsonRpcRequest = {
+            jsonrpc: "2.0",
+            method,
+            params: [],
+            id: uniqueId
+        };
+        
+        return new Promise((resolve: ResponseResolver, reject) => {
+            const requestString = jsonRpcRequest.toString();
+            const requestBuffer = Buffer.from(requestString);
+
+            this.pendingRequests.set(uniqueId, resolve);
+            this.socket.write(requestBuffer);
+        });
+    }
+
+    /** Socket listens for incoming requests, but those requests are responses of already sent requests */
+    private _listenForResponses() {
+        this.socket.setEncoding("utf8");
+
+        this.socket.on("data", (data: Buffer) => {
+            const chunk = data.toString();
+            const responses = chunk.split("\n");
+
+            for (const response of responses) {
+                this._handleResponse(response);
             }
-        })
+        });
     }
 
-    public connect(addr: Address, transport: ITransport): TcpClient {
-        return new TcpClient(addr, transport);
+    /** When response is returned, it is parsed, before deleting from the Map checks if exists and then resolves */
+    private _handleResponse(response: string) {
+        try {
+            const { id, result }: JsonRpcResponse = JSON.parse(response);
+        
+            if(!this.pendingRequests.has(id)) return;
+
+            const resolve = this.pendingRequests.get(id) as ResponseResolver;
+
+            this.pendingRequests.delete(id);
+
+            resolve(result);
+        } catch (error) {
+            throw error;            
+        }
     }
 
-    public send(cb: Callback): void {
-        this.transportServer.receive(cb);
+    /** Generates random id for requests */
+    private _generateRequestId() {
+        const alphabet = 'bjectSymhasOwnProp-0123456789ABCDEFGHIJKLMNQRTUVWXYZ_dfgiklquvxz';
+        let size = 10;
+        let id = '';
+
+        while (0 < size--) { id += alphabet[(Math.random() * 64) | 0]; }
+
+        return id;
     }
 
-    public receive(cb: Callback): void {
-        this.transportServer.send(cb);
-    }
 }
